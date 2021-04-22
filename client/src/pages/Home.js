d90e6d545e5ceb7ad5cd0eec43ec77cbab88efeb
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 12:54:04 -0400

    imported HeroJumbotron in the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,19 +2,22 @@ import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import CreatePostForm from "../components/CreatePostForm";
 import PostsList from "../components/PostsList";
-
+import HeroJumbotron from "../components/HeroJumbotron";
 const Home = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col size="md-6">
-          <CreatePostForm />
-        </Col>
-        <Col size="md-6 sm-12">
-          <PostsList />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <HeroJumbotron />
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <CreatePostForm />
+          </Col>
+          <Col size="md-6 sm-12">
+            <PostsList />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
