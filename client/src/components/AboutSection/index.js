82420a20ced824a66c7e20e54f577e009d741b4f
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 18:34:32 -0400

    created AboutSection

---
+++ client/src/components/AboutSection/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function AboutSection() {
+  return (
+    <div>
+      <h4>Welcome To Polar Cleaning</h4>
+      <h2>Cleaning Services in Columbus</h2>
+      <p>
+        Polar Cleaning is an Columbus cleaning company that specialises in
+        professional domestic & commercial services. We'll leave your house or
+        commercial space looking clean, comfortable and inviting. Our mission at
+        Polar Cleaning is to provide exceptional customer service and result
+        driven cleaning solutions for our clients. The team of experienced
+        professionals at PolarCleaning includes the best carpet cleaners, end of
+        lease cleaners, tile & grout cleaning specialists, asbestos removal
+        specialists, graffiti removal experts, curtain cleaners & blind cleaners
+        in Columbus.
+      </p>
+    </div>
+  );
+}
