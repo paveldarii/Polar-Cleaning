c7844875d86a473cd4a85512d81cffd6cd406a6c
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 18:24:30 -0400

    imported GeneralInfo component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,16 +3,19 @@ import { Col, Row, Container } from "../components/Grid";
 import CreatePostForm from "../components/CreatePostForm";
 import PostsList from "../components/PostsList";
 import HeroJumbotron from "../components/HeroJumbotron";
+import GeneralInfo from "../components/GeneralInfo";
+
 const Home = () => {
   return (
     <>
       <HeroJumbotron />
+      <GeneralInfo />
       <Container fluid>
         <Row>
           <Col size="md-6">
             <CreatePostForm />
           </Col>
-          <Col size="md-6 sm-12">
+          <Col size="md-6">
             <PostsList />
           </Col>
         </Row>
