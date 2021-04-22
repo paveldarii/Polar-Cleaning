621cec7d0d0cdd91f5315e83250fe09e4d69b6a5
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 12:54:24 -0400

    small change in Jumbotron

--- client/src/components/HeroJumbotron/index.js
+++ client/src/components/HeroJumbotron/index.js
@@ -3,7 +3,7 @@ import { Jumbotron, Button } from "react-bootstrap";
 export default function HeroJumbotron() {
   return (
     <Jumbotron>
-      <h1>Cleaning Services for Every Home and Office in Columbus, OH</h1>
+      <h3>Cleaning Services for Every Home and Office in Columbus, OH</h3>
       <ul>
         <li>Easy Booking and Quality Cleaning.</li>
         <li>We Clean Your Space, Not Your Wallet</li>
