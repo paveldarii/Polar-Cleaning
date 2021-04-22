ba5274cbfae2bdce8641d827f4d0ef068e2f83b8
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 12:49:09 -0400

    created the basic structure of the HomeJumbotron

---
+++ client/src/components/HeroJumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Jumbotron, Button } from "react-bootstrap";
+export default function HeroJumbotron() {
+  return (
+    <Jumbotron>
+      <h1>Cleaning Services for Every Home and Office in Columbus, OH</h1>
+      <ul>
+        <li>Easy Booking and Quality Cleaning.</li>
+        <li>We Clean Your Space, Not Your Wallet</li>
+        <li>Our Services are 100% Ensured.</li>
+      </ul>
+      <p>
+        <Button variant="primary">Get a Free Quote</Button>
+      </p>
+    </Jumbotron>
+  );
+}
