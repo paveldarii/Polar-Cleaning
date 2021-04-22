0cf0ce70af5ca7aad8353ea837ebd6296ae4100c
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 12:00:46 -0400

    added GetQuote component

--- client/src/components/CreatePostForm/index.js
+++ client/src/components/CreatePostForm/index.js
@@ -9,21 +9,21 @@ function CreatePostForm() {
   const authorRef = useRef();
   const [state, dispatch] = useStoreContext();
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     dispatch({ type: LOADING });
     API.savePost({
       title: titleRef.current.value,
       body: bodyRef.current.value,
-      author: authorRef.current.value
+      author: authorRef.current.value,
     })
-      .then(result => {
+      .then((result) => {
         dispatch({
           type: ADD_POST,
-          post: result.data
+          post: result.data,
         });
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
 
     titleRef.current.value = "";
     bodyRef.current.value = "";
@@ -39,10 +39,28 @@ function CreatePostForm() {
       </div>
       <h1>Create a blog post</h1>
       <form className="form-group mt-5 mb-5" onSubmit={handleSubmit}>
-        <input className="form-control mb-5" required ref={titleRef} placeholder="Title" />
-        <textarea className="form-control mb-5" required ref={bodyRef} placeholder="Body" />
-        <input className="form-control mb-5" ref={authorRef} placeholder="Screen name" />
-        <button className="btn btn-success mt-3 mb-5" disabled={state.loading} type="submit">
+        <input
+          className="form-control mb-5"
+          required
+          ref={titleRef}
+          placeholder="Title"
+        />
+        <textarea
+          className="form-control mb-5"
+          required
+          ref={bodyRef}
+          placeholder="Body"
+        />
+        <input
+          className="form-control mb-5"
+          ref={authorRef}
+          placeholder="Screen name"
+        />
+        <button
+          className="btn btn-success mt-3 mb-5"
+          disabled={state.loading}
+          type="submit"
+        >
           Save Post
         </button>
       </form>
