a05f7566ecaf284ae9b7c3ff44761fe3d8e75305
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 19:31:29 -0400

    added raw data for ServiceCard component

---
+++ client/src/assets/services.js
@@ -0,0 +1,34 @@
+import cleaningServiceImg from "./ServiceCardImg/cleaning-service.jpeg";
+const services = [
+  {
+    img: cleaningServiceImg,
+    title: "Cleaning Service",
+    description:
+      "We offer an extensive variety of cleaning services such as one-off cleaning and end of tenancy cleaning.",
+  },
+  {
+    img: cleaningServiceImg,
+    title: "House Cleaning",
+    description:
+      "Let the professionals clean your house! Polar Cleaning provides a weekly regular house cleaning service in Columbus, Ohio.",
+  },
+  {
+    img: cleaningServiceImg,
+    title: "Commercial Cleaning",
+    description:
+      "We meet all the required norms expected to productively clean any sort of business.",
+  },
+  {
+    img: cleaningServiceImg,
+    title: "Carpet Cleaning",
+    description:
+      "Carpet Cleaning Service Offered by Cleaning Pro is high quality and safe for both residential and offices.",
+  },
+  {
+    img: cleaningServiceImg,
+    title: "Upholstery Cleaning",
+    description:
+      "Our sofa cleaning experts are also experienced in rug and mattress cleaning.",
+  },
+];
+export default services;
