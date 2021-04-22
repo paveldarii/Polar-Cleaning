b3cb98414036308bac695f5f9420651289cb966c
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 18:41:28 -0400

    created ServiceCard component

---
+++ client/src/components/ServiceCard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+export default function ServiceCard(props) {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.img} />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>{props.description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
