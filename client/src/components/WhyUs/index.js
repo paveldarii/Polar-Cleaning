370714f721effe68bb9c7c5dccc69983b381605f
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 20:32:29 -0400

    mapped over whyUsReasons

--- client/src/components/WhyUs/index.js
+++ client/src/components/WhyUs/index.js
@@ -1,6 +1,25 @@
 import React from "react";
-import { Container } from "../Grid";
+import whyUsReasons from "../../assets/whyUsReasons";
+import WhyUsCard from "../WhyUsCard";
+import Wrapper from "../Wrapper";
+import { Container } from "react-bootstrap";
 
 export default function WhyUs() {
-  return <Container></Container>;
+  return (
+    <>
+      <Wrapper>
+        <h2>Why Choose us</h2>
+        <p>
+          There are many reasons why Polar Cleaning is the right choice for our
+          clients. Here are the top few reasons to give us a try:
+        </p>
+
+        {whyUsReasons.map((reason) => {
+          return (
+            <WhyUsCard title={reason.title} description={reason.description} />
+          );
+        })}
+      </Wrapper>
+    </>
+  );
 }
