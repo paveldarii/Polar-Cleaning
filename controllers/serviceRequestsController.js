d6bc02b89e7ccdc96124d35f6fa62a413f5ca5c2
Pavel Darii <paveldarii@yahoo.com>
2021-04-23 15:51:33 -0400

    created controllers for serviceRequest model

---
+++ controllers/serviceRequestsController.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+
+// Defining methods for the postsController
+module.exports = {
+  findAll: function (req, res) {
+    db.ServiceRequest.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.ServiceRequest.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.ServiceRequest.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.ServiceRequest.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.ServiceRequest.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
