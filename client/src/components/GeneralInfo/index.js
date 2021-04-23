92aa64b24f5d7875a0a3249635f5134dd8d23f7a
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 22:30:00 -0400

    imported HomeInfo component

--- client/src/components/GeneralInfo/index.js
+++ client/src/components/GeneralInfo/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import rawInfo from "../../assets/homeInfo";
+import HomeInfo from "../HomeInfo";
 
 export default function GeneralInfo() {
   return (
-    <Container>
-      <Row>
-        <Col md={4}>We are open Mon-Fri 8:30AM-5:30PM</Col>
-        <Col md={4}>Cal us now 6146413785</Col>
-        <Col md={4}>Email us polarcleaningllc@gmail.com</Col>
-      </Row>
-    </Container>
+    <>
+      {rawInfo.map((info) => {
+        return (
+          <HomeInfo
+            title={info.title}
+            description={info.description}
+          ></HomeInfo>
+        );
+      })}
+    </>
   );
 }
