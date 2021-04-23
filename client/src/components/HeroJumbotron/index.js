931452a1308fd449bcd5c6f5779f6cff818872ce
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 22:30:31 -0400

    setted up a new hero image

--- client/src/components/HeroJumbotron/index.js
+++ client/src/components/HeroJumbotron/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Jumbotron, Button } from "react-bootstrap";
+import globalStyle from "../../utils/GlobalStyle";
 export default function HeroJumbotron() {
   return (
-    <Jumbotron>
-      <h3>Cleaning Services for Every Home and Office in Columbus, OH</h3>
+    <Jumbotron style={globalStyle.homeHero}>
+      <h4>Cleaning Services for Every Home and Office in Columbus, OH</h4>
       <ul>
         <li>Easy Booking and Quality Cleaning.</li>
         <li>We Clean Your Space, Not Your Wallet</li>
