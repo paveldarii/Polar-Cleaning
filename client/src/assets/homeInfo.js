c6462ce73f4ced3c7a12f84e22fb6103552b600c
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 22:24:41 -0400

    created raw info file

---
+++ client/src/assets/homeInfo.js
@@ -0,0 +1,16 @@
+const homeInfo = [
+  {
+    title: "We are open",
+    description: "Mon-Fri 8:30AM-5:30PM",
+  },
+  {
+    title: "Call us Now",
+    description: "(614) 641-3785",
+  },
+  {
+    title: "Email us",
+    description: "polarcleaningllc@gmail.com",
+  },
+];
+
+export default homeInfo;
