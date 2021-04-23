c32cf5acb073bbdd945adf672ff9ea3deec7d390
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 20:30:28 -0400

    added raw data for the WhyUsCard components

---
+++ client/src/assets/whyUsReasons.js
@@ -0,0 +1,24 @@
+const reasons = [
+  {
+    title: "Highly-trained Staff",
+    description:
+      "Our cleaners are professionally trained and are experienced. We perform background checks to make sure that our clients have peace of mind when they hire us.",
+  },
+  {
+    title: "Online Booking",
+    description:
+      "Our cleaning services are available  with upfront pricing and secure payment 24 hours, 7 days a week. You can select a preferred delivery date and time slot at checkout.",
+  },
+  {
+    title: "Satisfaction Guaranteed",
+    description:
+      "Customer satisfaction is our primary motive. If you are not happy with our cleaning service, please call us within 24 hours and we’ll organise cleaners to go back and finish the job.",
+  },
+  {
+    title: "Fast Service",
+    description:
+      "Call Polar Cleaning if you need last minute cleaning requirements. We usually have cleaners on standby and will be able to start on short notice.",
+  },
+];
+
+export default reasons;
