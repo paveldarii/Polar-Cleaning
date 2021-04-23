e39c4e26e2d958ad82ce7297009145c128e3cf74
Pavel Darii <paveldarii@yahoo.com>
2021-04-22 20:31:27 -0400

    added why us card component

---
+++ client/src/components/WhyUsCard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export default function WhyUsCard(props) {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>{props.description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
