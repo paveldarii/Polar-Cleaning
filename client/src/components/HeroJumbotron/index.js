94b2075b1cbeb8ce3df24b230f611cb010a44739
Pavel Darii <paveldarii@yahoo.com>
2021-04-24 20:34:28 -0400

    changed the structure and styling of the hero jumbotron

--- client/src/components/HeroJumbotron/index.js
+++ client/src/components/HeroJumbotron/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Jumbotron, Button } from "react-bootstrap";
+import { Jumbotron, Button, ListGroup, Col } from "react-bootstrap";
 import globalStyle from "../../utils/GlobalStyle";
 export default function HeroJumbotron() {
   return (
     <Jumbotron style={globalStyle.homeHero}>
-      <h4>Cleaning Services for Every Home and Office in Columbus, OH</h4>
-      <ul>
-        <li>Easy Booking and Quality Cleaning.</li>
-        <li>We Clean Your Space, Not Your Wallet</li>
-        <li>Our Services are 100% Ensured.</li>
-      </ul>
+      <h2>Cleaning Services</h2>
+      <h2>for Every Home and Office</h2>
+      <h2>in Columbus, OH</h2>
+      <Col style={{ padding: "0px", fontSize: "20px" }} md={6}>
+        <p>Easy Booking and Quality Cleaning.</p>
+        <p>We Clean Your Space, Not Your Wallet</p>
+        <p>Our Services are 100% Ensured.</p>
+      </Col>
       <p>
         <Button variant="primary" href="/booking">
           Get a Free Quote
