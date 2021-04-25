30038973a72550453162a7f057a8624fe8b31689
Pavel Darii <paveldarii@yahoo.com>
2021-04-24 20:29:38 -0400

    created and exported more css variables

--- client/src/utils/GlobalStyle.js
+++ client/src/utils/GlobalStyle.js
@@ -1,14 +1,34 @@
 import homeHero from "../assets/images/home-hero.jpeg";
-var themeColor = "#3c99dc";
-var whiteText = "white";
+var themeColor = "white";
 
 const style = {
   themeColor: { backgroundColor: themeColor },
   navBarTextStyle: {
-    color: whiteText,
     fontFamily: "Times New Roman",
   },
-  homeHero: { backgroundImage: `url(${homeHero})`, backgroundSize: "cover" },
+  homeHero: {
+    //backgroundImage: `url(${homeHero})`,
+    fontFamily: "Times New Roman",
+    backgroundColor: "#bcd7ec",
+    backgroundSize: "cover",
+    margin: 0,
+    height: "60vh",
+    color: "#515151",
+  },
 };
-
+export const centerText = {
+  textAlign: "center",
+};
+export const secondaryTheme = {
+  backgroundColor: "#27bbdc",
+};
+export const tertialTheme = {
+  backgroundColor: "#354a5d",
+};
+export const quaternaryTheme = {
+  backgroundColor: "#f4f2f2",
+};
+export const titleColor = {
+  color: "#e49038",
+};
 export default style;
