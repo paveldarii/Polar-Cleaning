edc5e9ba3a453ac8a5ccc3a040a1d3d92d3f064d
Pavel Darii <paveldarii@yahoo.com>
2021-04-27 08:37:54 -0400

    imported new images, and added them to the assets list

--- client/src/assets/services.js
+++ client/src/assets/services.js
@@ -1,4 +1,7 @@
 import cleaningServiceImg from "./ServiceCardImg/cleaning-service.jpeg";
+import houseCleaning from "./ServiceCardImg/house-cleaning.jpeg";
+import commercialCleaning from "./ServiceCardImg/commercial-cleaning.jpeg";
+import carpetCleaning from "./ServiceCardImg/carpet-cleaning.jpeg";
 const services = [
   {
     img: cleaningServiceImg,
@@ -7,28 +10,28 @@ const services = [
       "We offer an extensive variety of cleaning services such as one-off cleaning and end of tenancy cleaning.",
   },
   {
-    img: cleaningServiceImg,
+    img: houseCleaning,
     title: "House Cleaning",
     description:
       "Let the professionals clean your house! Polar Cleaning provides a weekly regular house cleaning service in Columbus, Ohio.",
   },
   {
-    img: cleaningServiceImg,
+    img: commercialCleaning,
     title: "Commercial Cleaning",
     description:
       "We meet all the required norms expected to productively clean any sort of business.",
   },
   {
-    img: cleaningServiceImg,
+    img: carpetCleaning,
     title: "Carpet Cleaning",
     description:
       "Carpet Cleaning Service Offered by Cleaning Pro is high quality and safe for both residential and offices.",
   },
-  {
-    img: cleaningServiceImg,
-    title: "Upholstery Cleaning",
-    description:
-      "Our sofa cleaning experts are also experienced in rug and mattress cleaning.",
-  },
+  // {
+  //   img: cleaningServiceImg,
+  //   title: "Upholstery Cleaning",
+  //   description:
+  //     "Our sofa cleaning experts are also experienced in rug and mattress cleaning.",
+  // },
 ];
 export default services;
