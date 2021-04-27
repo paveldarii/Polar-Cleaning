13bd9475ce6a614736a8f00cad0f14c9b661797b
Pavel Darii <paveldarii@yahoo.com>
2021-04-27 09:19:29 -0400

    transformed the whole card into a link

--- client/src/components/ServiceCard/index.js
+++ client/src/components/ServiceCard/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Card, Image } from "react-bootstrap";
+import { useHistory } from "react-router";
+
 export default function ServiceCard(props) {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/booking");
+  };
   return (
-    <Card style={{ width: "18rem" }}>
-      <Image roundedCircle variant="top" src={props.img} />
+    <Card onClick={handleClick} style={{ width: "18rem", cursor: "pointer" }}>
+      <Image roundedCircle variant="top" src={props.img} alt={props.title} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.description}</Card.Text>
