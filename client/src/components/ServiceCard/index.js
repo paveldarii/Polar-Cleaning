d526c06e336745c566c486e9f3b7418ff6a4c5dd
Pavel Darii <paveldarii@yahoo.com>
2021-04-27 08:38:29 -0400

    changed the look of the Service Card

--- client/src/components/ServiceCard/index.js
+++ client/src/components/ServiceCard/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Image } from "react-bootstrap";
 export default function ServiceCard(props) {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={props.img} />
+      <Image roundedCircle variant="top" src={props.img} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.description}</Card.Text>
